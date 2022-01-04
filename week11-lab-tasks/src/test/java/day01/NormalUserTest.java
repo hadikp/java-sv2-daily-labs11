09fd5b9374da230899c999d6b344f8b7644e8fc5
hadikp <hp20001025@gmail.com>
2022-01-04 15:59:14 +0100

    day01 NormalUser and AdminUser

--- week11-lab-tasks/src/test/java/day01/NormalUserTest.java
+++ week11-lab-tasks/src/test/java/day01/NormalUserTest.java
@@ -7,10 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class NormalUserTest {
 
     @Test
-    void testUser() {
+    void testNormalUser() {
         NormalUser normalUser = new NormalUser("hadikp@freemail.hu", "aegt55");
-        System.out.println(normalUser.getUserEmail());
-        System.out.println(normalUser.getPassword());
+        assertEquals("hadikp@freemail.hu", normalUser.getUserEmail());
+        assertEquals("A kért jelszó: ******", normalUser.getPassword());
+    }
+
+    @Test
+    void testAdminUser(){
+        AdminUser adminUser = new AdminUser("hadikp@freemail.hu", "12345");
+        assertEquals("*****", adminUser.getPassword());
     }
 
 }
