7c4ad7c3bb179f896c6b8d136234c9fc5126ce7b
hadikp <hp20001025@gmail.com>
2022-01-05 19:27:18 +0100

    day02 Hiking test plus

--- week11-lab-tasks/src/test/java/day02/HikingTest.java
+++ week11-lab-tasks/src/test/java/day02/HikingTest.java
@@ -1,5 +1,6 @@
 package day02;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,11 +11,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HikingTest {
 
+    Hiking hiking;
+
+    @BeforeEach
+    void init() {
+        hiking = new Hiking();
+    }
+
     @Test
     void testGetElevation() {
-        Hiking hiking = new Hiking();
         List<Double> elevations = new ArrayList(Arrays.asList(10.0, 20.0, 15.0, 18.0));
         assertEquals(13.0, hiking.getPlusElevation(elevations));
+    }
+
+    @Test
+    void testFullElevationIncreasing() {
+        List<Double> elevations = new ArrayList(Arrays.asList(10.0, 20.0, 25.0, 30.0, 31.0));
+        assertEquals(21.0, hiking.getPlusElevation(elevations));
+    }
+
+    @Test
+    void testFullElevationDecreasing() {
+        List<Double> elevations = new ArrayList(Arrays.asList(31.0, 30.0, 20.0, 10.0, 3.0));
+        assertEquals(0.0, hiking.getPlusElevation(elevations));
     }
 
 }
